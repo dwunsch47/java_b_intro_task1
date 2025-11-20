ab4d56b0901b48aaa348e290603cfb8984b253a2
Anton <csengerhk@yandex.ru>
2025-11-20 21:47:18 +0700

    Customs calc

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-public class Main {
+import java.util.Scanner;
+import ru.netology.service.CustomsService;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Anton Kulikov");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите цену товара (в руб.): ");
+        int price = scanner.nextInt();
+
+        System.out.print("Введите вес товара (в кг.): ");
+        int weight = scanner.nextInt();
+
+        System.out.print("Размер пошлины (в руб.) составит: " + CustomsService.calculateCustoms(price, weight));
     }
 }
