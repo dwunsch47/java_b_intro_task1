90eec7f44f0b6587a0a39dd4a0f883eedba8e84a
Anton <csengerhk@yandex.ru>
2025-11-24 21:53:01 +0700

    Self check num. 1

--- Main.java
+++ Main.java
@@ -1,15 +1,35 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
-import ru.netology.service.CustomsService;
 
 class Main {
+
+    public static int checkNumOfDays(int year) {
+        return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) ? 366 : 365;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите цену товара (в руб.): ");
-        int price = scanner.nextInt();
 
-        System.out.print("Введите вес товара (в кг.): ");
-        int weight = scanner.nextInt();
-
-        System.out.print("Размер пошлины (в руб.) составит: " + CustomsService.calculateCustoms(price, weight));
+        System.out.println("Choose mode: check or game");
+        String mode = scanner.nextLine();
+        if (mode.equals("check")) {
+            System.out.println("Введите год в формате yyyy");
+            int year = scanner.nextInt();
+            System.out.println("Количество дней " + checkNumOfDays(year));
+        } else if (mode.equals("game")) {
+            int score = 0;
+            while (true) {
+                System.out.println("Введите год");
+                int year = scanner.nextInt();
+                System.out.println("Введите количество дней");
+                int days = scanner.nextInt();
+                if (days != checkNumOfDays(year)) {
+                    System.out.println("Неправильно! В этом году " + checkNumOfDays(year));
+                    System.out.println("Набрано очков: " + score);
+                    break;
+                }
+                score++;
+            }
+        }
     }
 }
