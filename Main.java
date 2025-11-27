e89ad0db26b76f1a3b5e9264192fabfcb17d54ad
Anton <csengerhk@yandex.ru>
2025-11-28 00:21:15 +0700

    Data types

--- Main.java
+++ Main.java
@@ -1,34 +1,58 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 class Main {
+    public static double INCOME_TAX = 0.06;
+    public static double DIFFERENCE_TAX = 0.15;
 
-    public static int checkNumOfDays(int year) {
-        return ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) ? 366 : 365;
+    public static int incomeTaxCalc(int income) {
+        return (int) (income * INCOME_TAX);
+    }
+
+    public static int differenceTaxCalc(int income, int spending) {
+        int difference = income - spending;
+        return (difference >= 0 ? (int) (difference * DIFFERENCE_TAX) : 0);
+    }
+
+    public static void chooseBestTax(int incomeTax, int differenceTax) {
+        if (incomeTax == differenceTax) {
+            System.out.println("Можете выбрать любую систему налогообложения");
+            return;
+        }
+        int taxDiff = incomeTax - differenceTax;
+        boolean isIncomeTaxBetter = taxDiff < 0;
+        System.out.println("\nМы советуем вам " + (isIncomeTaxBetter ? "УСН доходы" : "УСН доходы минус расходы"));
+        System.out.println("Ваш налог составит: " + (isIncomeTaxBetter ? incomeTax : differenceTax));
+        System.out.println("Налог на другой системе: " + (!isIncomeTaxBetter ? incomeTax : differenceTax));
+        System.out.println("Экономия: " + Math.abs(taxDiff));
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Choose mode: check or game");
-        String mode = scanner.nextLine();
-        if (mode.equals("check")) {
-            System.out.println("Введите год в формате yyyy");
-            int year = scanner.nextInt();
-            System.out.println("Количество дней " + checkNumOfDays(year));
-        } else if (mode.equals("game")) {
-            int score = 0;
-            while (true) {
-                System.out.println("Введите год");
-                int year = scanner.nextInt();
-                System.out.println("Введите количество дней");
-                int days = scanner.nextInt();
-                if (days != checkNumOfDays(year)) {
-                    System.out.println("Неправильно! В этом году " + checkNumOfDays(year));
-                    System.out.println("Набрано очков: " + score);
+        Scanner sc = new Scanner(System.in);
+        int income = 0;
+        int spending = 0;
+        while (true) {
+            System.out.println("\nВыберите операцию и введите её номер:");
+            System.out.println("1. Добавить новый доход\n2. Добавить новый расход\n3. Выбрать систему налогообложения");
+            System.out.print("Введите команду: ");
+            String command = sc.nextLine();
+            if (command.equals("end")) {
+                break;
+            }
+            switch (command) {
+                case "1":
+                    System.out.print("Введите сумму дохода: ");
+                    income += Integer.parseInt(sc.nextLine());
                     break;
-                }
-                score++;
+                case "2":
+                    System.out.print("Введите сумму раходов: ");
+                    spending += Integer.parseInt(sc.nextLine());
+                    break;
+                case "3":
+                    chooseBestTax(incomeTaxCalc(income), differenceTaxCalc(income, spending));
+                    break;
+                default:
+                    System.out.println("Неправильная команда");
+                    break;
             }
         }
     }
