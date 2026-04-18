53aa17b0ce19c1522ec8d691b3e5757fde68eab0
Anton K. <kulikov.anton.dev@proton.me>
2026-04-19 02:34:49 +0700

    First task

--- Main.java
+++ Main.java
@@ -1,40 +1,71 @@
-import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main() {
-        Map<Address, Integer> costPerAddress = new HashMap<>();
-        Set<String> uniqueCountries = new HashSet<>();
-        int overallCost = 0;
+    public static void main(String[] args) {
+        StringBuilder log = new StringBuilder(); // could be global, but might be a bit ambiguous
 
-        costPerAddress.put(new Address("Россия", "Москва"), 1000);
-        costPerAddress.put(new Address("Россия", "Санкт-Петербург"), 750);
-        costPerAddress.put(new Address("Чехия", "Прага"), 2500);
+       File basePath = new File("D:\\Games");
+       dirCreator(basePath, log);
 
-        Scanner scanner = new Scanner(System.in);
-        String country, city;
-        int weight, cost;
-        Address currentOrder;
-        while (true) {
-            System.out.print("Заполнение нового заказа.\nВведите страну: ");
-            country = scanner.nextLine();
-            if (country.equals("end")) break;
-            System.out.print("Введите город: ");
-            city = scanner.nextLine();
-            if (city.equals("end")) break;
-            uniqueCountries.add(country); // поздно т.к. пользователь может до этого момента отменить
-            System.out.print("Введите вес (кг): ");
-            weight = Integer.parseInt(scanner.nextLine());
-            currentOrder = new Address(country, city);
-            if (!costPerAddress.containsKey(currentOrder)) {
-                System.out.println("Доставки по этому адресу нет");
-            } else {
-                cost = weight * costPerAddress.get(currentOrder);
-                overallCost += cost;
-                System.out.println("Стоимость доставки составит: " + cost + " руб.");
-            }
-            System.out.println("Общая стоимость всех доставок: " + overallCost + " руб.");
-            System.out.println("Количество уникальных стран: " + uniqueCountries.size());
-            System.out.println();
+       //src
+        File srcPath = new File(basePath, "src");
+        dirCreator(srcPath, log);
+
+       File testPath = new File(srcPath, "test");
+       dirCreator(testPath, log);
+
+       File mainPath = new File(srcPath, "main");
+       dirCreator(mainPath, log);
+
+       // src/main
+       File mainFilePath = new File(mainPath, "Main.java");
+       fileCreator(mainFilePath, log);
+
+       File utilsFilePath = new File(mainPath, "Utils.java");
+       fileCreator(utilsFilePath, log);
+
+       // savegames
+        File saveGamesPath = new File(basePath, "savegames");
+        dirCreator(saveGamesPath, log);
+
+        // res
+        File resPath = new File(basePath, "res");
+        dirCreator(resPath, log);
+
+        File drawablesPath = new File(resPath, "drawables");
+        dirCreator(drawablesPath, log);
+
+        File vectorsPath = new File(resPath, "vectors");
+        dirCreator(vectorsPath, log);
+
+        File iconsPath = new File(resPath, "icons");
+        dirCreator(iconsPath, log);
+
+        File tmpPath = new File(basePath, "temp");
+        dirCreator(tmpPath, log);
+        File tmpFilePath = new File(tmpPath, "temp.txt");
+        fileCreator(tmpFilePath, log);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(tmpFilePath))) {
+            bw.write(log.toString());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void logger(File file, boolean status, StringBuilder builder) {
+        builder.append("\"").append(file.getAbsoluteFile()).append(status ? "\" was created successfully" : " wasn't created");
+        builder.append('\n');
+    }
+
+    public static void dirCreator(File toCreate, StringBuilder log) {
+        logger(toCreate, toCreate.mkdir(), log);
+    }
+
+    public static void fileCreator(File toCreate, StringBuilder log) {
+        try {
+            logger(toCreate, toCreate.createNewFile(), log);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
